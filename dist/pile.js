d8ec4bb7b693862b7c89fe6670dd6bf05fe316ef
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-09-03 11:32:19 +0200

    feat: Pile algorithm

--- dist/pile.js
+++ dist/pile.js
@@ -5,8 +5,31 @@ class Pile {
     add(item) {
         this.table.push(item);
     }
+    delete() {
+        if (!this.isEmpty())
+            return this.table.pop();
+        else
+            return "La Pile est vide";
+    }
+    peek() {
+        if (!this.isEmpty())
+            return this.table[this.size() - 1];
+        else
+            "La Pile est vide";
+    }
+    size() {
+        return this.table.length;
+    }
+    isEmpty() {
+        return this.size() === 0;
+    }
 }
 const test = new Pile();
 test.add(15);
 test.add(25);
-console.log(test.table);
+test.add(125);
+test.add(5);
+test.add(255);
+console.log(test.peek());
+let res = test.delete();
+console.log(test.isEmpty());
