cc265b2af4d9864e3c7c2480b99c724186e2572e
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-01-10 22:35:09 +0100

    feat: binary search algorithm

--- dist/algo.js
+++ dist/algo.js
@@ -8,12 +8,38 @@ export default class Algo {
     isEmpty() {
         return this.size() === 0;
     }
-    contain(item) {
+    linearSearch(item) {
         for (let i = 0; i < this.size(); i++) {
             if (this.table[i] === item)
                 return true;
         }
         return false;
+    }
+    binarySearch(item, left, right) {
+        if (Math.abs(right - left) === 1) {
+            if (item === this.table[left] || item === this.table[right]) {
+                return true;
+            }
+            else {
+                return -1;
+            }
+        }
+        else {
+            let divided = Math.floor((left + right) / 2);
+            if (item === this.table[divided]) {
+                return true;
+            }
+            else if (item < this.table[divided]) {
+                let newLeft = left;
+                let newRight = divided;
+                this.binarySearch(item, newLeft, newRight);
+            }
+            else {
+                let newLeft = divided;
+                let newRight = right;
+                this.binarySearch(item, newLeft, newRight);
+            }
+        }
     }
     clear() {
         this.table = [];
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-let obj = {
-    name: 'Arno',
-    age: 23,
-};
+import Pile from './pile.js';
 
-console.log(obj);
+let tab = new Pile();
+tab.add(1);
+tab.add(12);
+tab.add(123);
+tab.add(1456);
+let result = tab.binarySearch(11, 1, 4);
+console.log(result);
